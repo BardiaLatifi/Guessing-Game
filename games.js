4f1c75250b2a668410301c8cac57ccfbbc8d7a5c
BardiaLatifi <Bardialatifi70@gmail.com>
2024-10-20 11:42:56 +0330

    adding games started

--- games.js
+++ games.js
@@ -91,7 +91,7 @@ const gamesArray = [
    new Game(20,"grade3", "MGS 4: Guns of the Patriots", 2008, ["Action-adventure", "Stealth"], "PlayStation", true,
                              "Konami", "Japan", "Solid Snake", "./assets/image20.jpg","7th gen"),
 
-   new Game(21,"grade1", "Alan Wake 2", 2000, "gener1", ["PC", "PlayStation", "Xbox"], false,
+   new Game(21,"grade1", "Alan Wake 2", 2000, "Survival/Horror", ["PC", "PlayStation", "Xbox"], false,
                              "Remedy Entertainment", "Finland", "Saga Anderson", "./assets/image0.jpg"),
 
                              
